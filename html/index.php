f1ac9464eef0061756a77663a4accc7db5905802
yuki-iwamoto12 <y.iwamoto.0012.git@gmail.com>
2023-05-11 22:27:15 +0900

    log

--- html/index.php
+++ html/index.php
@@ -1,4 +1,4 @@
 <?php
-echo ("aaaa");
+echo ("docker log test");
 file_put_contents('php://stdout', 'This is a log message.' . "\n");
 file_put_contents('php://stderr', 'This is an error log message.' . "\n");
